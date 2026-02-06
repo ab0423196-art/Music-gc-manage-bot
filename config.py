fe363f3b58a7079c46ac3ea1ebb5887137b0d455
ab0423196-art <ab0423196@gmail.com>
2026-02-06 23:22:58 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ from pyrogram import filters
 load_dotenv()
 
 # ── Core bot config ────────────────────────────────────────────────────────────
-API_ID = int(getenv("API_ID", 27798659))
-API_HASH = getenv("API_HASH", "26100c77cee02e5e34b2bbee58440f86")
+API_ID = int(getenv("API_ID", 23264133))
+API_HASH = getenv("API_HASH", "945e5b76ce8550bebbeeaf5599e7ce58")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
-OWNER_ID = int(getenv("OWNER_ID", 7660990923))
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Lord_Vasudev_Krishna")
-BOT_USERNAME = getenv("BOT_USERNAME", "Noelamusicbot")
-BOT_NAME = getenv("BOT_NAME", "𝐍ᴏᴇʟᴀ 𝝌 𝐌ᴜsɪᴄ ♪")
-ASSUSERNAME = getenv("ASSUSERNAME", "˹𝐍ᴏᴇʟᴀ ✘ 𝐀ssɪsᴛᴀɴᴛ˼")
+OWNER_ID = int(getenv("OWNER_ID", 6883111123))
+OWNER_USERNAME = getenv("OWNER_USERNAME", "Eren_Yeager_76")
+BOT_USERNAME = getenv("BOT_USERNAME", "AB_VERSE_MUSIC_BOT")
+BOT_NAME = getenv("BOT_NAME", "AB VERSE MUSIC BOT")
+ASSUSERNAME = getenv("ASSUSERNAME", "˹Kaneki")
 
 # ── Database & logging ─────────────────────────────────────────────────────────
 MONGO_DB_URI = getenv("MONGO_DB_URI")
-LOGGER_ID = int(getenv("LOGGER_ID", -1002014167331))
+LOGGER_ID = int(getenv("LOGGER_ID", -1003533680480))
 
 # ── Limits (durations in min/sec; sizes in bytes) ──────────────────────────────
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 300))
